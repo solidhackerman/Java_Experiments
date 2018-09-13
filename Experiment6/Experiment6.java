884a0a30290771defb97e981e29e9d38faa6782f
hackermansolid <42809764+hackermansolid@users.noreply.github.com>
2018-09-13 12:05:45 +0530

    Experiment6 added

---
+++ Experiment6/Experiment6.java
@@ -0,0 +1,31 @@
+class Experiment6
+{
+	public static void main(String[] args)
+	{
+		String s1 = new String("Vihaan ");
+		String s2 = s1.concat("Sharma");
+		String s3 = s2.toLowerCase();
+		String s4 = s3.toUpperCase();
+		String s5 = s4.substring(2);
+		System.out.println(" ");
+		System.out.println("Original String");
+		System.out.println("------------------------------\n"+s1);
+		System.out.println(" ");
+		System.out.println("Concatenated String");
+		System.out.println("------------------------------\n"+s2);
+		System.out.println(" ");
+		System.out.println("LowerCase String");
+		System.out.println("------------------------------\n"+s3);
+		System.out.println(" ");
+		System.out.println("UpperCase String");
+		System.out.println("------------------------------\n"+s4);
+		System.out.println(" ");
+		System.out.println("Substring String");
+		System.out.println("------------------------------\n"+s5);
+	}
+}
+		
+		
+		
+		
+		
